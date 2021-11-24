5d234457add8982d22c394432b81562269fa49bd
engineerkong <89781823+engineerkong@users.noreply.github.com>
2021-11-24 17:45:32 +0100

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,8 @@
+     $ $ # Make sure you are in the correct directory
+    # Make sure you are in the correct directory
+    $ $ cd CalculatorLibrary
+    cd CalculatorLibrary
+    $ git add calculator.py
+    $ git add calculator.py
+    $ git commit -m $ git commit -m "Add functions for addition and subtraction"
+    "Add functions for addition and subtraction"
